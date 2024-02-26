13888f77b019364b23e36db10f5249794dc75066
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-26 16:22:35 +0100

    [donig]bonus+fix

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ Dato un array contenente una lista di cinque immagini, creare un carosello come 
 ## Svolgimento
 
 1. creare container
-2. 
+2. creare array
+3. creare div img con js
+4. impostare i tasti
+5. assegnare ai tasti il comando per switchare le immagini
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const imgWrapper = document.querySelector('.img-wrapper');
 const up = document.querySelector('.up');
 const down = document.querySelector('.down');
+const imagesList = document.querySelector('.list-img')
 
 up.classList.add('hide');
 
@@ -17,10 +18,14 @@ const images = [
 for(let i = 0; i < images.length; i++){
   const img = images[i];
   imgWrapper.innerHTML += `<img class="img hide" src="${img}" >`;
+  
+  // imagesList.innerHTML += `<img class="images-list" src="${img}" >`;
 }
 
 const itemsCollection = document.getElementsByClassName('img');
 itemsCollection[0].classList.remove('hide');
+
+// imagesList[counterImg].classList.add('active');
 
 // tatso up
 up.addEventListener('click', function(){
@@ -50,3 +55,5 @@ down.addEventListener('click', function(){
 
 
 })
+
+
